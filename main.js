ffc04e886126c0b54c9a9643866334923129af3b
MelroyNoronha <melmax1208@gmail.com>
2017-09-21 20:46:58 +0530

    cleaned code

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ var mid = canvas.width / 2;
 var bottom = canvas.height - 30;
 var puckX = mid;
 var puckY = innerHeight / 2;
-var gravity = 1;
 
 pen.canvas.addEventListener("click", function(event) {
   var mouseX = event.clientX;
@@ -25,11 +24,9 @@ pen.canvas.addEventListener("click", function(event) {
     puck.moveUp();
   }
   if (mouseY < puckY + 30) {
-    pen.clearRect(0, 0, innerWidth, innerHeight);
+    puck.clearPuck();
     puck.drawPuck();
   }
-
-  console.log("clicked");
 });
 
 var puck = {
@@ -39,42 +36,43 @@ var puck = {
     pen.fillStyle = "white";
     pen.fillRect(puckX, puckY, 30, 30);
   },
-  moveLeft: function() {
+  clearPuck: function() {
     pen.clearRect(0, 0, innerWidth, innerHeight);
+  },
+  moveLeft: function() {
+    this.clearPuck();
     puckX += 50;
     this.drawPuck();
   },
   moveRight: function() {
-    pen.clearRect(0, 0, innerWidth, innerHeight);
+    this.clearPuck();
     puckX -= 50;
     this.drawPuck;
   },
   moveUp: function() {
-    pen.clearRect(0, 0, innerWidth, innerHeight);
+    this.clearPuck();
     puckY -= 50;
     this.drawPuck();
   }
 };
 
-function keepInsideCanvas() {
+function declareGameOver() {
   if (puckY > canvas.height - 30) {
-    puckY -= 1;
+    puckArea.innerHTML = "Game Over";
   }
   if (puckX > canvas.width - 30) {
-    puckX -= 1;
+    puckArea.innerHTML = "Game Over";
   }
 }
 
 function emulateGravity() {
   puck.drawPuck();
   puckY += 2;
-  keepInsideCanvas();
+  declareGameOver();
   requestAnimationFrame(emulateGravity);
-  console.log("emulateGravity running");
+  setInterval(function() {
+    puck.clearPuck();
+  }, 1000 / 60);
 }
-
-setInterval(function() {
-  pen.clearRect(0, 0, innerWidth, innerHeight);
-}, 1000 / 60);
 
 emulateGravity();
